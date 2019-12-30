368c1d061208eb768ebcf9c4956c63aae92196b2
Aaron <aaronlam1004@gmail.com>
2019-12-29 23:07:08 -0800

    Add files via upload

--- lib/execute_vagr.py
+++ lib/execute_vagr.py
@@ -20,19 +20,6 @@ if os.path.exists('Vagr.json'):
         folders = [folder[0] for folder in vagr_json['shared']]
         if sys.argv[2] not in folders:
             subprocess.run(['VBoxManage', 'sharedfolder', 'add', vagr_json['machine'], '--name', sys.argv[2], '--hostpath', sys.argv[3], '--automount'])
-    elif sys.argv[1] == '--init_shared':
-        for folder in vagr_json['shared']:
-            if folder[0] == sys.argv[2]:
-                if folder[1] == False:
-                    org_folder = '/media/sf_' + sys.argv[2]
-                    new_folder = '/home/buddy/' + sys.argv[2]
-                    subprocess.run(['VBoxManage', '--nologo', 'guestcontrol', vagr_json['machine'], 'run', '--exe', '/bin/ln', '--username', 'buddy', '--password', '1234567890', '--wait-stdout', '--', 'ln/arg0', '-s', org_folder, new_folder])
-                    vagr_json['shared'][vagr_json['shared'].index(folder)][1] = True
-                    json_file.close()
-                    os.chdir(cwd)
-                    with open('Vagr.json', 'w') as (outfile):
-                        json.dump(vagr_json, outfile)
-                    break
     elif sys.argv[1] == '--del_shared':
         folders = [folder[0] for folder in vagr_json['shared']]
         for folder in folders:
